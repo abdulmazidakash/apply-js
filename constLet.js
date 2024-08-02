b063e6d12777423b11193a583c1c7fce52c225ef
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-02 15:55:50 +0600

    learning const let

---
+++ constLet.js
@@ -0,0 +1,22 @@
+// var name = "javed akver";
+// console.log(name.length);
+
+var name = 'dav';
+if ( name.length >4 ){
+    name ='juvu';
+}
+
+console.log(name);
+
+// var age =15;
+console.log(name);
+let age =15;
+age = 16;
+age =17;
+console.log(age);
+// var country ='bangladesh';
+const country ='bangladesh';
+console.log(country);
+
+// note : const mean constant 
+// let 
