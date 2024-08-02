444cdb881ffd98b5e906cb872cdfdd4fe2dffa33
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-02 18:28:54 +0600

    learning factorial recursive

---
+++ factorialRecursive.js
@@ -0,0 +1,19 @@
+// for(var i =10; i>= 1; i--){
+
+// }
+// var i = 10;
+// while (i <= 1 ){
+//     i--;
+// }
+
+function factorial(n){
+    if(n == 0){
+        return 1;
+    }
+    else{
+        return n * factorial(n-1);
+    }
+}
+
+var result = factorial(10);
+console.log(result);
