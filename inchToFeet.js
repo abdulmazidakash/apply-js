60a9610308c340391d64376dc0d6e169765041ab
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-02 15:22:04 +0600

    apply js inchToFeet added

---
+++ inchToFeet.js
@@ -0,0 +1,21 @@
+// var inch = 156;
+// var feet = inch / 12;
+
+// console.log(feet);
+// console.log(feet);
+// console.log(feet);
+
+function inchToFeet(inch){
+    var feet = inch / 12;
+    return feet;
+}
+var senior = [156, 288, 300];
+
+var nanaFeet = inchToFeet(senior[0]);
+
+var nanaFeet = inchToFeet(156);
+console.log(nanaFeet);
+var naniFeet = inchToFeet(288);
+console.log(naniFeet);
+var dadiFeet = inchToFeet(300);
+console.log(dadiFeet);
